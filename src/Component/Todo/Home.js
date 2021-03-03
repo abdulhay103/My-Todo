f99a5eeb0dc4153fa37145c3189c881a73617ca2
Abdul Hay <mdabdulhay95@gmail.com>
2021-03-03 21:12:10 +0600

    Todo working

--- src/Component/Todo/Home.js
+++ src/Component/Todo/Home.js
@@ -1,36 +1,43 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [name, setname] = useState("");
+  const [developer, setDeveloper] = useState({
+    name: "",
+    details: "",
+  });
+  const { name, details } = developer;
   const [todos, setTodos] = useState([]);
 
-  const onChangeHandler = (e) => {
-    const value = e.target.value;
-    setname(value);
-  };
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    setTodos([...name, name]);
-    setname("");
-    console.log(todos);
-    // const develperName = name;
-    // console.log(developName);
+    setTodos([...todos, { name, details }]);
+    console.log(developer);
   };
   return (
     <div>
       <h1>Developer Details Receiver</h1>
       <form onSubmit={onSubmitHandler}>
         <label htmlFor="name">Developer Nmae: </label>
-        <input type="text" value={name} onChange={onChangeHandler} />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setDeveloper({ name: e.target.value })}
+        />
         <br />
         <label htmlFor="details">Developer Details: </label>
-        <input type="text" />
+        <input
+          type="text"
+          value={details}
+          onChange={(e) => setDeveloper({ details: e.target.value })}
+        />
         <br />
         <button type="submit">Submit</button>
       </form>
       {todos.map((todo, index) => {
-        return <h1 key={index}>{todo}</h1>;
+        // console.log(todo.name);
+        return <h1 key={index}>{todo.name}</h1>;
       })}
+      {/* {console.log(todos)} */}
     </div>
   );
 };
